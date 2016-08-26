56b2cca4c09838f5736a46f15a8e7e78e3b3fa16
Freeman <Steven.Freeman@mindshift.com>
2016-08-26 12:45:14 -0400

    Added more questions, still working through ideas

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,16 +17,35 @@ var questions = [
 		option3: "<div class='option' id = '3'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, U-God, Raekwon da Chef, Cappadonna, Method Man</div>",
 		option4: "<div class='option' id = '4'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, Masta Kill, Raekwon da Chef, Sonny Cheeba, Method Man</div>",
 		correctAnswerDisplay: "<p>The Wu-Tang Clan is an American hip hop group from New York City, originally composed of East Coast rappers RZA, GZA, Method Man, Raekwon, Ghostface Killah, Inspectah Deck, U-God, Masta Killa, and Ol' Dirty Bastard. The group was formed in and is associated with the New York City borough of Staten Island. The Wu-Tang Clan has released seven gold and platinum studio albums with worldwide sales of more than 40 million albums, including members' solo projects, since 1992. Its debut album, Enter the Wu-Tang (36 Chambers), is considered to be one of the greatest albums in hip-hop history and it is said to have been groundbreaking for hip-hop and one of the most important albums of the hip-hop golden age</p>",
-		image: "../images/biggie.jpg"
+		image: "../images/wutangclan.jpg"
 	},
 	question3 = {
-		question: "<h4>Who were the original 9 members of the Wu Tang Clan?</h4>",
-		option1: "<div class='option' id = '1'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, Masta Killa, Raekwon da Chef, U-God, Method Man</div>",
-		option2: "<div class='option' id = '2'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, Masta Killa, Raekwon da Chef, Cappadonna, Method Man</div>",
-		option3: "<div class='option' id = '3'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, U-God, Raekwon da Chef, Cappadonna, Method Man</div>",
-		option4: "<div class='option' id = '4'>The RZA, The GZA, Ol' Dirty Bastard, Inspectah Deck, Ghostface Killa, Masta Kill, Raekwon da Chef, Sonny Cheeba, Method Man</div>",
-		correctAnswerDisplay: "<p>The Wu-Tang Clan is an American hip hop group from New York City, originally composed of East Coast rappers RZA, GZA, Method Man, Raekwon, Ghostface Killah, Inspectah Deck, U-God, Masta Killa, and Ol' Dirty Bastard. The group was formed in and is associated with the New York City borough of Staten Island. The Wu-Tang Clan has released seven gold and platinum studio albums with worldwide sales of more than 40 million albums, including members' solo projects, since 1992. Its debut album, Enter the Wu-Tang (36 Chambers), is considered to be one of the greatest albums in hip-hop history and it is said to have been groundbreaking for hip-hop and one of the most important albums of the hip-hop golden age</p>",
-		image: "../images/biggie.jpg"
+		question: "<h4>What sub-genre of rap do rappers Young Thug, Rich Homie Quan and Future fall under?</h4>",
+		option1: "<div class='option' id = '1'>Trap Rap</div>",
+		option2: "<div class='option' id = '2'>Party Rap</div>",
+		option3: "<div class='option' id = '3'>Mumble Rap</div>",
+		option4: "<div class='option' id = '4'>R&B</div>",
+		correctAnswerDisplay: "<p>Mumble rap is a style of rapping in which the rappers are hard to understand. They focus on emotion and melody moreso than lyrical content and have been criticized as a bastardizaiton of the rap genre as a whole.</p>",
+		image: "../images/thugga.jpg"
+	}
+	question4 = {
+		question: "<h4>What does each letter stand for in KRS-ONE's name?</h4>",
+		option1: "<div class='option' id = '1'>Krishna's Regarded Son 1</div>",
+		option2: "<div class='option' id = '2'>Krips Run Supreme Over Nearly Everyone</div>",
+		option3: "<div class='option' id = '3'>Knowledge Reigns Supreme Over Nearly Everyone</div>",
+		option4: "<div class='option' id = '4'>Krazy Righteous Super One</div>",
+		correctAnswerDisplay: "<p>KRS-One AKA Lawrence Parker was born in The Bronx, New York in 1965. Parker left home at 16 to become an MC, coming to live at a homeless shelter in the South Bronx where he was dubbed 'Krisna' by residents because of his interest in the Hare Krishna spirituality of some of the antipoverty workers. By the time he met youth counselor Scott Sterling, he was also writing graffiti as KRS-One (Knowledge Reigns Supreme Over Nearly Everyone). Together he and Sterling, a.k.a. DJ Scott La Rock eventually created Boogie Down Productions, releasing their debut album, Criminal Minded, in 1987.</p>",
+		image: "../images/KRS.jpg"
+	}
+	question5 = {
+		question: "<h4>What does C.R.E.A.M stand for?</h4>",
+		option1: "<div class='option' id = '1'>Crips Run Everything And More</div>",
+		option2: "<div class='option' id = '2'>Cash Rules Everything Around Me</div>",
+		option3: "<div class='option' id = '3'>Criminal Rights East And Maple</div>",
+		option4: "<div class='option' id = '4'>Cash Robbery Extortion And Money</div>",
+		correctAnswerDisplay: "<p>'Cash Rules Everything Around Me! CREAM! Get the money, dolla dolla bill ya'll'- Wu Tang Clan</p>",
+		image: "../images/cream.jpg",
+		song: "../sounds/cream.mp3"
 	}
 ]
 //Variable that tracks which question number we are on. 
